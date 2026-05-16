47fd9ca4a841fe0d5304ca42ad1c0329914ecc9b
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-05-16 16:41:41 -0700

    3rd dropdown

--- public/app.py
+++ public/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
-import webbrowser
+import csv
 
 app = Flask(__name__)
+
+file = open("static/classes.csv", mode = "r")
+
+classes = csv.reader(file)
 
 @app.route("/")
 def index():
@@ -11,4 +15,4 @@ def index():
 
 @app.route("/schedule")
 def schedule():
-    return render_template("schedule.html")
+    return render_template("schedule.html", classes = classes)
